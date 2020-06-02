c8261e96f00a8b17c7048f682bec4305fd1a192f
Jan Rodolf Espinas <jrpespinas@gmail.com>
2020-06-02 23:37:39 +0800

    chore: solve problem 1

--- problems/multiples_of_3_and_5.py
+++ problems/multiples_of_3_and_5.py
@@ -5,5 +5,9 @@
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+def multiples_of_3_and_5(n = 1000):
+    return sum([i for i in range(1,n) if (i%3 == 0) or (i%5 == 0)])
+
 if __name__ == "__main__":
-    pass
+    print(multiples_of_3_and_5(1000))
+    
